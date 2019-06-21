ffbd53d92d3982a1573a8b0835607a4d79355556
Arindam Lahiri <arindam.lahiri@gmail.com>
2019-06-20 22:24:14 -0400

    First Release

---
+++ app.js
@@ -0,0 +1,3 @@
+var math = require('./math.js');
+console.log(math.add(10,2));
+console.log(math.sub(10,3));
---
+++ math.js
@@ -0,0 +1,7 @@
+var exports = module.exports={};
+exports.add = function(a,b){
+    return a+b;
+};
+exports.sub = function(a,b){
+    return a - b;
+};
